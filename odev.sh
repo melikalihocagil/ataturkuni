df0e1bd9d93735cb998bb1eb1a839940adca46f7
Melik Alihocagil <79751540+melikalihocagil@users.noreply.github.com>
2024-01-17 17:03:07 +0300

    odev.sh olusturuldu

---
+++ odev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ad="Ahmet Melik"
+soyad="Alihocagil"
+numara="Alihocagil"
+
+toplam=$(( $1 + $2 ))
+echo ""
+echo ""
+echo "Merhaba ben $ad $soyad numarali ogrencinizim"
+echo "$toplam"
+echo " "
+echo "Hocam biliyorum cok gec gonderiyorum ama projeden haberim oldugunda"
+echo "o an icerisinde yapabilecek durumda degildim yeni yapip size yololayabilirum"
+echo "projemi degerlendirip yuksek verebilirseniz cok mutlu olurum"
+echo "vizem 100 finalimden de yuksek aldim"
+echo "ikinci ogeretimdim sabah derslerine katilabilmem genelde cok mumkun olmuyordu"
+echo "acik birakip izlememek de istemezdim dersinizi dinledigim anlarda kendimi vererek dionledigimi soylemek isterim"
+echo "proje verdiginizden de gecen hafta ogrendim derse katilamadigim icin"
+echo "vizeden 100 aldigimi ise daha bugun ogrenebildim hocam hatta"
+echo "hatta derste bizzat benim adimi vermissiniz"
+echo "ayrica verdiginiz emeklerden dolayi cok tesekkur ederim hocam"
